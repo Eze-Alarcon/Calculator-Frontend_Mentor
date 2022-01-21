79fc97f9fe5e53454003702b95d681bc7c7e3dfa
ezequiel <ps3ezequiel@gmail.com>
2022-01-21 18:03:47 +0100

    errores encontrados, iniciando nuevo commit para fix

--- code/main.js
+++ code/main.js
@@ -8,7 +8,7 @@ let logs = {
     record0: "",
     record1: "",
     record2: "",
-    accion: ""
+    action: ""
 }
 
 // ===== Event Listeners =====
@@ -39,7 +39,7 @@ function show(event) {
             || event.target.dataset.type === "dot") {
                 calculate(event.target)
         } else {
-            action(event.target)
+            otherKeys(event.target)
         }
     }
     // console.log("primer log", logs)
@@ -65,21 +65,82 @@ function calculate(event) {
     // console.log("segundo log", logs)
 }
 
-// Casos de botones de action
 
-function action(event) {
+// Casos de botones NO numericos
+function otherKeys(event) {
+    (event.dataset.type === "action") 
+        ? actionButtons(event)
+        : specialButtons(event)
+}
 
-    if (event.dataset.type === "action") {
-        console.log("este valor es = ", event.dataset.value)
-    } else {
-        // console.log("estos son especiales: ", event.dataset.value)
-        del()
+function actionButtons(event) {
+    switch(event.dataset.value) {
+        case "+":
+            sum()
+            break
+        case "-":
+            minus()
+            break
+        case "/":
+            divide()
+            break
+        case "x":
+            multiply()
     }
 }
+
+function sum() {
+    logs.record1 =  Number(logs.record1) + Number(logs.record0)
+    logs.record0 = ""
+    screen.innerHTML = "+"
+    console.log(logs)
+}
+
+function minus() {
+    (logs.record1 === "")
+     ? logs.record1 =  Number(logs.record0)
+     : logs.record1 =  Number(logs.record1) - Number(logs.record0)
+    logs.record0 = ""
+    screen.innerHTML = "-"
+    console.log(logs)
+}
+
+function divide() {
+     if (logs.record1 === "") {
+        logs.record1 =  Number(logs.record0)
+    } else if (logs.record0 !== "") {
+        logs.record1 =  Number(logs.record1) / Number(logs.record0)
+    }
+    logs.record0 = ""
+    screen.innerHTML = "/"
+    console.log(logs)
+}
+
+function specialButtons(event) {
+    switch(event.dataset.value) {
+        case "del":
+            del()
+            break
+        case "reset":
+            reset()
+            break
+        case "equal":
+            console.log("equal")
+            break
+    }
+}
+
 
 function del() {
     logs.record0 = logs.record0.slice(0, (logs.record0.length - 1))
     screen.innerHTML = logs.record0
 }
 
-/* puedo usar logs.record0.length = 0 para hacer que se resetee  */
+
+function reset() {
+    logs.record0 = ""
+    logs.record1 = ""
+    logs.record2 = ""
+    logs.action = ""
+    screen.innerHTML = logs.record0
+}
