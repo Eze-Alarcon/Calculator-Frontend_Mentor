417bdb8a5eef9b315b233aacd2c52ec9c2363b49
ezequiel <ps3ezequiel@gmail.com>
2022-01-24 20:21:10 +0100

    Fix mayores listos, empezando con Fix menores

--- code/main.js
+++ code/main.js
@@ -2,49 +2,40 @@
 
 let pad = document.getElementById("keyPad")
 let screen = document.getElementById("screen")
+let screenInfo = document.getElementById("screen--data")
 let screenValue = 0
 
 let logs = {
     record0: "",
     record1: "",
-    record2: "",
-    action: ""
+    action: "",
+    status: ""
 }
 
 // ===== Event Listeners =====
 
 
-pad.addEventListener("click", show)
+pad.addEventListener("click", press)
 
 
 // ===== Functions =====
 
 
-// Funcion inicial y validacion de tipo de boton presionado
-function show(event) {
+function press(event) {
     if (event.target.dataset.value !== undefined) {
-        
         event.target.classList.toggle("click")
         setTimeout(function ()  {
             event.target.classList.toggle("click")
         }.bind(event), 100)
-
-
-        if (screen.dataset.initial === "true" 
-            && event.target.dataset.type === "num") {
-                screen.dataset.initial = "false"
-                screen.innerHTML = `${logs.record0}${event.target.dataset.value}`
-                logs.record0 = `${logs.record0}${event.target.dataset.value}`
-        } else if (event.target.dataset.type === "num" 
-            || event.target.dataset.type === "dot") {
-                calculate(event.target)
-        } else {
-            otherKeys(event.target)
-        }
     }
-    // console.log("primer log", logs)
-}
+    if (event.target.dataset.type === "num" 
+        || event.target.dataset.type === "dot") {
+            calculate(event.target)
+    } else {
+        otherKeys(event.target)
+    }
 
+}
 
 
 // Casos de numeros y dot
@@ -54,15 +45,16 @@ function calculate(event) {
         screenValue = `${logs.record0}${event.dataset.value}`
         screen.innerHTML = screenValue
         logs.record0 = screenValue
+        screenInfo.innerHTML = `${logs.record0}`
 
 
     } else if (event.dataset.type === "dot" 
         && !logs.record0.includes(".")) {
             screenValue = logs.record0 + "."
             screen.innerHTML = screenValue
+            screenInfo.innerHTML = `${logs.record0 + "."}`
             logs.record0 = screenValue
-    } 
-    // console.log("segundo log", logs)
+    }
 }
 
 
@@ -73,48 +65,19 @@ function otherKeys(event) {
         : specialButtons(event)
 }
 
+
 function actionButtons(event) {
-    switch(event.dataset.value) {
-        case "+":
-            sum()
-            break
-        case "-":
-            minus()
-            break
-        case "/":
-            divide()
-            break
-        case "x":
-            multiply()
+    logs.action = event.dataset.value
+    
+    if (logs.record1 !== "") {
+        equal()
+    } else {
+        logs.record1 = logs.record0
+        logs.record0 = ""
     }
 }
 
-function sum() {
-    logs.record1 =  Number(logs.record1) + Number(logs.record0)
-    logs.record0 = ""
-    screen.innerHTML = "+"
-    console.log(logs)
-}
 
-function minus() {
-    (logs.record1 === "")
-     ? logs.record1 =  Number(logs.record0)
-     : logs.record1 =  Number(logs.record1) - Number(logs.record0)
-    logs.record0 = ""
-    screen.innerHTML = "-"
-    console.log(logs)
-}
-
-function divide() {
-     if (logs.record1 === "") {
-        logs.record1 =  Number(logs.record0)
-    } else if (logs.record0 !== "") {
-        logs.record1 =  Number(logs.record1) / Number(logs.record0)
-    }
-    logs.record0 = ""
-    screen.innerHTML = "/"
-    console.log(logs)
-}
 
 function specialButtons(event) {
     switch(event.dataset.value) {
@@ -125,8 +88,7 @@ function specialButtons(event) {
             reset()
             break
         case "equal":
-            console.log("equal")
-            break
+            equal()
     }
 }
 
@@ -134,13 +96,60 @@ function specialButtons(event) {
 function del() {
     logs.record0 = logs.record0.slice(0, (logs.record0.length - 1))
     screen.innerHTML = logs.record0
+    screenInfo.innerHTML = `${logs.record0}`
 }
 
 
 function reset() {
     logs.record0 = ""
     logs.record1 = ""
-    logs.record2 = ""
     logs.action = ""
-    screen.innerHTML = logs.record0
+    screen.innerHTML = ""
+    screenInfo.innerHTML = ''
+    screen.dataset.initial = "true"
+}
+
+
+function equal() {
+
+    let num0 = Number(logs.record0)
+    let num1 = Number(logs.record1)
+
+    let calc = `${logs.record1} ${logs.action} ${logs.record0}`
+
+    switch (logs.action) {
+        case "+": 
+            logs.record0 = num1 + num0
+            logs.status = "ok"
+            break
+        case "-":
+            logs.record0 = num1 - num0
+            logs.status = "ok"
+            break
+        case "/":
+            divide(num0, num1)
+            break
+        case "x":
+            logs.record0 = (num1 * num0).toFixed(0)
+            logs.status = "ok"
+    }
+    if (logs.status === "ok") {
+        if (Number(logs.record0) < 0) logs.record0.toFixed(2)
+        screenInfo.innerHTML = `${calc} = ${logs.record0}`
+        screen.innerHTML = `${logs.record0}`
+        logs.record1 = ""
+    } else {
+        screenInfo.innerHTML = ""
+        screen.innerHTML = "Syntax Error"
+    }
+}
+
+function divide(num0, num1) {
+    console.log(logs.record0 !== "0")
+    if (logs.record0 !== "0") {
+        logs.record0 = num1 / num0
+        logs.status = "ok"        
+    } else {
+        logs.status = "invalid"
+    }
 }
